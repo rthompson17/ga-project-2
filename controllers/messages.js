786ca0c4c45d2133a599e25ace3fef484f1891d6
username <youremail@domain.com>
2022-04-10 23:00:28 -0400

    routes and controllers working hoorayyy

--- controllers/messages.js
+++ controllers/messages.js
@@ -8,14 +8,28 @@ module.exports = {
     sendMessage
 }
 
-function newMatch() {
-
-}
-
-function create() {
-
-}
-
+//// CLOSE EYE ON THIS ONE...
 function sendMessage() {
+    // Match.findById(req.params.id, function(err, reviewerDocument){
+    //     reviewerDocument.reviewerMessage.push(req.body.messageId);
+    //     reviewerDocument.save(function(err){
+    //         res.redirect(`/matches/${reviewerDocument.__id}`)
+    //     })
+    // })
+}
 
+/// CLOSE EYE ON THIS...
+function create(req, res) {
+    Message.create(req.body, function (err, message) {
+        res.redirect('/messages/new');
+    });
+}
+
+function newMatch(req, res) {
+    Message.find({}, function (err, messages) {
+        res.render('messages/new', {
+            title: 'Send Direct Message',
+            messages
+        });
+    })
 }
