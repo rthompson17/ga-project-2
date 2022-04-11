8731967a18dfef17fe675e785a52ad66d2709025
username <youremail@domain.com>
2022-04-10 20:05:52 -0400

    bootstrap installed

--- server.js
+++ server.js
@@ -63,6 +63,26 @@ app.use('/', reviewsRouter);
 app.use('/', messagesRouter);
 app.use('/', indexRouter);
 
+// /// bootstrap middleware...
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+)
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/matches/index.html"))
+})
+
+app.listen(5000, () => {
+  console.log("Listening on port " + 5000)
+})
+// /// end of bootstrap middleware
+
 
 // invalid request, send 404 page
 app.use(function(req, res) {
